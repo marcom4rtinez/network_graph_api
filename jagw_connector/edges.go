21f19ca35fed6d887fe24dd3f82987731c481e0d
Marco Martinez <mmmartinez@bluewin.ch>
2023-06-04 17:54:26 -0600

    stability improvements

--- jagw_connector/edges.go
+++ jagw_connector/edges.go
@@ -3,17 +3,30 @@ package jagw_connector
 import (
 	"context"
 	"log"
+	"time"
 
 	"github.com/jalapeno-api-gateway/jagw-go/jagw"
 )
 
 func GetAllLinks() *jagw.LsNodeEdgeResponse {
+	var response *jagw.LsNodeEdgeResponse
+	var err error
+
 	client := getJagwRequestClient()
 	defer closeJagwRequestClient()
+
 	request := &jagw.TopologyRequest{}
-	response, err := client.GetLsNodeEdges(context.Background(), request)
+
+	for i := 0; i < 3; i++ {
+		response, err = client.GetLsNodeEdges(context.Background(), request)
+		if err == nil {
+			break
+		}
+		time.Sleep(2 * time.Second)
+	}
+
 	if err != nil {
-		log.Fatalf("Error when calling GetLsLinks on request service: %s", err)
+		log.Fatalf("Error when calling GetLsNodeEdges on request service: %s", err)
 	}
 	return response
 }
--- jagw_connector/nodes.go
+++ jagw_connector/nodes.go
@@ -3,16 +3,28 @@ package jagw_connector
 import (
 	"context"
 	"log"
+	"time"
 
 	"github.com/jalapeno-api-gateway/jagw-go/jagw"
 )
 
 func GetAllNodes() *jagw.LsNodeResponse {
+	var response *jagw.LsNodeResponse
+	var err error
+
 	client := getJagwRequestClient()
 	defer closeJagwRequestClient()
+
 	request := &jagw.TopologyRequest{}
 
-	response, err := client.GetLsNodes(context.Background(), request)
+	for i := 0; i < 3; i++ {
+		response, err = client.GetLsNodes(context.Background(), request)
+		if err == nil {
+			break
+		}
+		time.Sleep(2 * time.Second)
+	}
+
 	if err != nil {
 		log.Fatalf("Error when calling GetLsNodes on request service: %s", err)
 	}
